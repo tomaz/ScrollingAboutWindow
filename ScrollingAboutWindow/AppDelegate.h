c3d9f3c1edb9b708c14b72717d903db96de1c1bd
Tomaz Kragelj <tkragelj@gmail.com>
2012-03-08 09:06:41 +0100

    Initial Commit

---
+++ ScrollingAboutWindow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScrollingAboutWindow
+//
+//  Created by Tomaž Kragelj on 3/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
